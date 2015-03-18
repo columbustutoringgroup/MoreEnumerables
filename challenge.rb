c56ed00ea34abcf0fccd03635f331cac8ebada76
ABCDEFG <abcdefg@ABCDEFGs-MacBook-Pro.local>
2015-03-18 14:17:57 -0400

    Same file, added 2 new CD animals

--- challenge.rb
+++ challenge.rb
@@ -20,21 +20,21 @@ def fetch_the_first_two(input)
 end
 
 def fetch_CD_animals(input)
-  input.grep /cat|dog/
+  input.select { |cditems| ('c'..'d').include? cditems[0]}
 end
 
 ## DO NOT CHANGE CODE BELOW THIS LINE ##
 
-animals = ["cat", "moose", "dog", "bird"]
+animals = ["cat", "moose", "dog", "bird", "canary", "dingo"]
 
-p capitalize_each_string(animals) == ["Cat", "Moose", "Dog", "Bird"]
+p capitalize_each_string(animals) == ["Cat", "Moose", "Dog", "Bird", "Canary", "Dingo"]
 
 p fetch_the_dog(animals) == ["dog"] 
 
-p no_dogs_allowed(animals) == ["cat", "moose", "bird"]
+p no_dogs_allowed(animals) == ["cat", "moose", "bird", "canary", "dingo"]
 
-p count_the_animals(animals) == 4
+p count_the_animals(animals) == 6
 
 p fetch_the_first_two(animals)  == ["cat", "moose"]
 
-p fetch_CD_animals(animals) == ["cat", "dog"]
+p fetch_CD_animals(animals) == ["cat", "dog", "canary", "dingo"]
