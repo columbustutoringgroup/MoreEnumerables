409b35a6dcdb563726b1b6b598c2c176cb23de08
Jonathan Arnett <JArnett@covermymeds.com>
2015-05-21 09:45:38 -0400

    It works!

--- challenge.rb
+++ challenge.rb
@@ -1,26 +1,25 @@
 
 def capitalize_each_string(input)
-  #implement your solution here
+  input.map{|x| x.capitalize}
 end
 
 def fetch_the_dog(input)
-  #implement your solution here
+  input.find_all{|x| x == "dog"}
 end
 
 def no_dogs_allowed(input)
-  #implement your solution here
+  input.select{|x| x != "dog"}
 end
 
 def count_the_animals(input)
-  #implement your solution here
+  input.count
 end
 
 def fetch_the_first_two(input)
-  #implement your solution here
+  input.first(2)
 end
 
 def fetch_CD_animals(input)
-  #implement your solution here
 end
 
 ## DO NOT CHANGE CODE BELOW THIS LINE ##
