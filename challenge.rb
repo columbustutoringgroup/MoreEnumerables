c15a49931692ab577f1c12ee581c89152cee4c8c
LazeeCoder <svenable88@yahoo.com>
2015-08-25 23:08:32 -0400

    Initial checkin

--- challenge.rb
+++ challenge.rb
@@ -1,26 +1,50 @@
 
 def capitalize_each_string(input)
   #implement your solution here
+  input.map{|value| value.capitalize}
 end
 
 def fetch_the_dog(input)
   #implement your solution here
+  input.select{|animal| animal == "dog"}
 end
 
 def no_dogs_allowed(input)
   #implement your solution here
+  animals = Array.new
+  for animal in input
+  	if animal != "dog"
+  		animals << animal
+  	end
+  end
+  return animals
 end
 
 def count_the_animals(input)
   #implement your solution here
+  input.count
 end
 
 def fetch_the_first_two(input)
   #implement your solution here
+  animals = Array.new
+  input.each_with_index	do |animal, idx|
+  	if idx == 0  || idx == 1
+		animals << input[idx]  	
+  	end
+  end
+  animals
 end
 
 def fetch_CD_animals(input)
   #implement your solution here
+  animals = Array.new
+  input.each do |animal|
+  	if animal.start_with? "c" or animal.start_with? "d"
+  		animals << animal
+  	end
+  end
+  animals
 end
 
 ## DO NOT CHANGE CODE BELOW THIS LINE ##
