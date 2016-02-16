9207573d39ad5c3e53dd36ff2fe962481a5e7d6d
shahmk <shahmk@mail.uc.edu>
2016-02-16 12:48:27 -0500

    Challenge Complete

--- challenge.rb
+++ challenge.rb
@@ -1,26 +1,32 @@
 
 def capitalize_each_string(input)
   #implement your solution here
+  return input.map { |e| e.capitalize }
 end
 
 def fetch_the_dog(input)
   #implement your solution here
+  return [].push(input.find {|e| e == "dog"})
 end
 
 def no_dogs_allowed(input)
   #implement your solution here
+  input.reject {|e| e == "dog"}
 end
 
 def count_the_animals(input)
   #implement your solution here
+  return input.count
 end
 
 def fetch_the_first_two(input)
   #implement your solution here
+  return input.first(2)
 end
 
 def fetch_CD_animals(input)
   #implement your solution here
+  return input.select {|e| e[0] == "c" or e[0] == "d"}
 end
 
 ## DO NOT CHANGE CODE BELOW THIS LINE ##
@@ -29,7 +35,7 @@ animals = ["cat", "moose", "dog", "bird"]
 
 p capitalize_each_string(animals) == ["Cat", "Moose", "Dog", "Bird"]
 
-p fetch_the_dog(animals) == ["dog"] 
+p fetch_the_dog(animals) == ["dog"]
 
 p no_dogs_allowed(animals) == ["cat", "moose", "bird"]
 
