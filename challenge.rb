537322ea09bb5dade117a5c8db0a05989700e103
dwin357 <e.r.steinmetz@gmail.com>
2016-06-10 10:00:47 -0400

    all cases passing true

--- challenge.rb
+++ challenge.rb
@@ -1,26 +1,30 @@
 
 def capitalize_each_string(input)
-  #implement your solution here
+  input.map{ |word| word.capitalize }
 end
 
 def fetch_the_dog(input)
-  #implement your solution here
+  #normally this would be a select, but this is working around the "only once" constraint
+  input.each_with_object([]){ |word, ary| ary << word if word.downcase == "dog"}
 end
 
 def no_dogs_allowed(input)
-  #implement your solution here
+  input.reject{ |word| word.downcase == "dog"}
 end
 
 def count_the_animals(input)
-  #implement your solution here
+  input.count
 end
 
 def fetch_the_first_two(input)
-  #implement your solution here
+  input.slice(0..1)
 end
 
 def fetch_CD_animals(input)
-  #implement your solution here
+  input.select do |word|
+    initial_letter = word[0].downcase
+    initial_letter == "c" || initial_letter == "d"
+  end
 end
 
 ## DO NOT CHANGE CODE BELOW THIS LINE ##
@@ -29,7 +33,7 @@ animals = ["cat", "moose", "dog", "bird"]
 
 p capitalize_each_string(animals) == ["Cat", "Moose", "Dog", "Bird"]
 
-p fetch_the_dog(animals) == ["dog"] 
+p fetch_the_dog(animals) == ["dog"]
 
 p no_dogs_allowed(animals) == ["cat", "moose", "bird"]
 
