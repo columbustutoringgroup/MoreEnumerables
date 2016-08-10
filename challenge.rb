057d4d8f732395777e1fc26b30f17749b49a020e
Jonathan Stevens <jstevens@covermymeds.com>
2016-08-10 14:01:15 -0400

    Jonathan Stevens submission

--- challenge.rb
+++ challenge.rb
@@ -1,26 +1,31 @@
 
 def capitalize_each_string(input)
-  #implement your solution here
+  input.map { |x| x.capitalize }
 end
 
 def fetch_the_dog(input)
-  #implement your solution here
+  input.select {|x| x == 'dog'}
 end
 
 def no_dogs_allowed(input)
-  #implement your solution here
+  input.reject {|x| x == 'dog'}
+  # alternative answers
+  # input - ['dog']
+  # input.select {|x| x != 'dog'}
 end
 
 def count_the_animals(input)
-  #implement your solution here
+  input.count
+  # alternative
+  # input.length
 end
 
 def fetch_the_first_two(input)
-  #implement your solution here
+  input[0..1]
 end
 
 def fetch_CD_animals(input)
-  #implement your solution here
+  input.select {|x| x.start_with?('c','d')}
 end
 
 ## DO NOT CHANGE CODE BELOW THIS LINE ##
@@ -29,7 +34,7 @@ animals = ["cat", "moose", "dog", "bird"]
 
 p capitalize_each_string(animals) == ["Cat", "Moose", "Dog", "Bird"]
 
-p fetch_the_dog(animals) == ["dog"] 
+p fetch_the_dog(animals) == ["dog"]
 
 p no_dogs_allowed(animals) == ["cat", "moose", "bird"]
 
