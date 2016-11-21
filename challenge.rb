b5548518c5b080955605719adb7c0e59a58e2b98
drm2 <davidrmyersii@gmail.com>
2016-11-21 13:39:07 -0500

    Updating challenge with passing logic.

--- challenge.rb
+++ challenge.rb
@@ -1,26 +1,26 @@
 
 def capitalize_each_string(input)
-  #implement your solution here
+  input.collect { |item| item.capitalize }
 end
 
 def fetch_the_dog(input)
-  #implement your solution here
+  input.select { |item| item == "dog" }
 end
 
 def no_dogs_allowed(input)
-  #implement your solution here
+    input.reject { |item| item == "dog" }
 end
 
 def count_the_animals(input)
-  #implement your solution here
+  input.count
 end
 
 def fetch_the_first_two(input)
-  #implement your solution here
+  input.slice(0, 2)
 end
 
 def fetch_CD_animals(input)
-  #implement your solution here
+  input.find_all { |item| item.start_with?("c", "d") }
 end
 
 ## DO NOT CHANGE CODE BELOW THIS LINE ##
