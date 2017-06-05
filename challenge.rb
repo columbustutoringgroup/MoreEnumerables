3a2b36cf2879db508d173f0cc33ba8ef46ab2f02
John Chester <jchester@cmmc02th17xg8wl.innova.local>
2017-06-05 15:30:14 -0400

    Jack Chester Solutions

--- challenge.rb
+++ challenge.rb
@@ -1,26 +1,30 @@
 
 def capitalize_each_string(input)
-  #implement your solution here
+  result = []
+  input.each_index{ |idx| result.push(input[idx].capitalize)}
+  return result
 end
 
-def fetch_the_dog(input)
-  #implement your solution here
+def fetch_the_dog(input)	
+  return [input.detect{ |word| word == "dog"}]
 end
 
 def no_dogs_allowed(input)
-  #implement your solution here
+  return input.reject{ |word| word == "dog"}
 end
 
 def count_the_animals(input)
-  #implement your solution here
+  return input.length
 end
 
 def fetch_the_first_two(input)
-  #implement your solution here
+  first_two = []
+  (0..1).each{ |idx| first_two.push(input[idx])}
+  return first_two
 end
 
 def fetch_CD_animals(input)
-  #implement your solution here
+  return input.select{ |word| word[0].upcase == 'C' || word[0].upcase == 'D'}
 end
 
 ## DO NOT CHANGE CODE BELOW THIS LINE ##
