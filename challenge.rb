70ead58d3fd598af2138c0122139a975a21c4c87
Victoria <vhall@covermymeds.com>
2017-06-16 12:19:11 -0400

    array challenge project

--- challenge.rb
+++ challenge.rb
@@ -1,26 +1,26 @@
 
 def capitalize_each_string(input)
-  #implement your solution here
+  return input.map{|input| input.capitalize}
 end
 
 def fetch_the_dog(input)
-  #implement your solution here
+  return input.select{|input| input == "dog"}
 end
 
 def no_dogs_allowed(input)
-  #implement your solution here
+  return input.reject{|input| input == "dog"}
 end
 
 def count_the_animals(input)
-  #implement your solution here
+  return input.count
 end
 
 def fetch_the_first_two(input)
-  #implement your solution here
+  return input[0..1]
 end
 
 def fetch_CD_animals(input)
-  #implement your solution here
+  return input.keep_if{|input| input == "cat" or input == "dog"}
 end
 
 ## DO NOT CHANGE CODE BELOW THIS LINE ##
@@ -29,7 +29,7 @@ animals = ["cat", "moose", "dog", "bird"]
 
 p capitalize_each_string(animals) == ["Cat", "Moose", "Dog", "Bird"]
 
-p fetch_the_dog(animals) == ["dog"] 
+p fetch_the_dog(animals) == ["dog"]
 
 p no_dogs_allowed(animals) == ["cat", "moose", "bird"]
 
