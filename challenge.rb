7de29a5d4cd0326e39642964fcc50463225f9241
Rupert Saxton <rsaxton@cmmc02vv0hchv2j.innova.local>
2018-01-18 15:48:58 -0500

    Pass all the tests

--- challenge.rb
+++ challenge.rb
@@ -1,26 +1,26 @@
 
 def capitalize_each_string(input)
-  #implement your solution here
+	input.map { | word | word.capitalize }
 end
 
 def fetch_the_dog(input)
-  #implement your solution here
+	input.find_all { | word | word == "dog" }
 end
 
 def no_dogs_allowed(input)
-  #implement your solution here
+	input.reject { | word | word == "dog" }
 end
 
 def count_the_animals(input)
-  #implement your solution here
+	input.count
 end
 
 def fetch_the_first_two(input)
-  #implement your solution here
+	input.take(2)
 end
 
 def fetch_CD_animals(input)
-  #implement your solution here
+	input.select { | word | word[0].downcase == "c" || word[0].downcase == "d" }
 end
 
 ## DO NOT CHANGE CODE BELOW THIS LINE ##
