53bcd78a523c0e55ec7bb33277e1cb3477a64fd1
Rupert Saxton <rsaxton@cmmc02vv0hchv2j.innova.local>
2018-01-18 16:12:31 -0500

    Apply all recommended changes

--- challenge.rb
+++ challenge.rb
@@ -1,26 +1,26 @@
 
 def capitalize_each_string(input)
-	input.map { | word | word.capitalize }
+  input.map(&:capitalize)
 end
 
 def fetch_the_dog(input)
-	input.find_all { | word | word == "dog" }
+  input.find_all { | word | word == "dog" }
 end
 
 def no_dogs_allowed(input)
-	input.reject { | word | word == "dog" }
+  input.reject { | word | word == "dog" }
 end
 
 def count_the_animals(input)
-	input.count
+  input.count
 end
 
 def fetch_the_first_two(input)
-	input.take(2)
+  input.take(2)
 end
 
 def fetch_CD_animals(input)
-	input.select { | word | word[0].downcase == "c" || word[0].downcase == "d" }
+  input.grep(/\A[cCdD]/)
 end
 
 ## DO NOT CHANGE CODE BELOW THIS LINE ##
