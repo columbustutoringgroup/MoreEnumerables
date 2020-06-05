c403ac6ac765182654c6b1ddeecb5f71d58b9d24
Eric Chen <echen@cmmc02c1256md6p.innova.local>
2020-06-05 14:09:00 -0400

    Completed the MoreEnumerables challenge

--- challenge.rb
+++ challenge.rb
@@ -1,35 +1,41 @@
 
 def capitalize_each_string(input)
-  #implement your solution here
+  input.map { |word|
+    letters = word.split('')
+    letters.first.upcase!
+    letters.join
+  }
 end
 
 def fetch_the_dog(input)
-  #implement your solution here
+  input.select {|animal| animal == "dog"}
 end
 
 def no_dogs_allowed(input)
-  #implement your solution here
+  input.reject {|animal| animal == "dog"}
 end
 
 def count_the_animals(input)
-  #implement your solution here
+   input.count()
 end
 
 def fetch_the_first_two(input)
-  #implement your solution here
+  copy = []
+  copy.append(input[0])
+  copy.append(input[1])
+  return copy
 end
 
 def fetch_CD_animals(input)
-  #implement your solution here
+  input.select{|a| a == "dog" or a == "cat"}
 end
 
 ## DO NOT CHANGE CODE BELOW THIS LINE ##
 
 animals = ["cat", "moose", "dog", "bird"]
-
 p capitalize_each_string(animals) == ["Cat", "Moose", "Dog", "Bird"]
 
-p fetch_the_dog(animals) == ["dog"] 
+p fetch_the_dog(animals) == ["dog"]
 
 p no_dogs_allowed(animals) == ["cat", "moose", "bird"]
 
